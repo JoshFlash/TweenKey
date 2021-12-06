e6a0f2d730a172442d137e97a7d1296883dc6825
Joshua Flash <joshuaflash@gmail.com>
2021-12-07 00:45:11 +1300

    tidy some code

--- Source/Interpolation/Easing.cs
+++ Source/Interpolation/Easing.cs
@@ -6,8 +6,6 @@ namespace TweenKey.Interpolation
     
     public static class Easing
     {
-        private const float OHM = 2.622057f;
-        
         public static EasingFunction Linear =>      x => x;
 
         public static class Quadratic
@@ -70,8 +68,8 @@ namespace TweenKey.Interpolation
             public static EasingFunction InOut =>   x => 
                 x == 0f ? 0f : Abs(x - 1f) < System.Single.Epsilon ? 1f : 
                 x < 0.5f 
-                    ? Pow(2f, 20f * x - 10f) / 2f 
-                    : (2f - Pow(2f, -20f * x + 10f)) / 2f;
+                    ? Pow(E, 20f * x - 10f) / 2f 
+                    : (2f - Pow(E, -20f * x + 10f)) / 2f;
         }
         
         public static class Circular
@@ -87,6 +85,8 @@ namespace TweenKey.Interpolation
         
         public static class Back
         {
+            private const float OHM = 2.622057f;
+
             public static EasingFunction In =>      x => (E - 1f) * x * x * x - E * x * x;
             public static EasingFunction Out =>     x => 1f + (E - 1f) * Pow(x - 1f, 3f) + E * Pow(x - 1f, 2f);
 
@@ -100,17 +100,17 @@ namespace TweenKey.Interpolation
         {
             public static EasingFunction In =>      x =>
                 x == 0f ? 0f : Abs(x - 1f) < System.Single.Epsilon ? 1f :
-                -Pow(2, 10 * x - 10) * Sin((x * 10f - 10.75f) * 2f * PI / 3f);
+                -Pow(E, 10f * x - 10f) * Sin((x * 10f - 10.75f) * 2f * PI / 3f);
             
             public static EasingFunction Out =>     x =>
                 x == 0f ? 0f : Abs(x - 1f) < System.Single.Epsilon ? 1f :
-                Pow(2, -10 * x) * Sin((x * 10f - 0.75f) * 2f * PI / 3f) + 1f;
+                Pow(E, -10f * x) * Sin((x * 10f - 0.75f) * 2f * PI / 3f) + 1f;
 
             public static EasingFunction InOut =>   x => 
                 x == 0f ? 0f : Abs(x - 1f) < System.Single.Epsilon ? 1f : 
                 x < 0.5f
-                    ? -(Pow(2f, 20f * x - 10f) * Sin((20f * x - 11.125f) * 4f * PI / 9f)) / 2f
-                    : Pow(2f, -20f * x + 10f) * Sin(20f * x - 11.125f) * 4f * PI / 9f / 2f + 1f;
+                    ? -(Pow(E, 20f * x - 10f) * Sin((20f * x - 11.125f) * 4f * PI / 9f)) / 2f
+                    : Pow(E, -20f * x + 10f) * Sin(20f * x - 11.125f) * 4f * PI / 9f / 2f + 1f;
         }
         
         public static class Bounce
@@ -118,10 +118,10 @@ namespace TweenKey.Interpolation
             public static EasingFunction In =>      x => 1f - Out(1f - x);
             
             public static EasingFunction Out =>     x =>
-                    x < 1f / 2.75f      ?   (121f / 16f) * (x * x)
-                :   x < 2f / 2.75f      ?   (121f / 16f) * (x -= 1.5f / 2.75f) * x + 0.75f
-                :   x < 2.5f / 2.75f    ?   (121f / 16f) * (x -= 2.25f / 2.75f) * x + 0.9375f
-                :                           (121f / 16f) * (x -= 2.625f / 2.75f) * x + 0.984375f;
+                    x < 4f / 11f    ?   (121f / 16f) * (x * x)
+                :   x < 8f / 11f    ?   (121f / 16f) * (x -= 6f / 11f) * x + 0.75f
+                :   x < 10f / 11f   ?   (121f / 16f) * (x -= 10f / 11f) * x + 0.9375f
+                :                       (121f / 16f) * (x -= 10.5f / 11f) * x + 0.984375f;
 
             public static EasingFunction InOut =>   x => 
                 x < 0.5f
