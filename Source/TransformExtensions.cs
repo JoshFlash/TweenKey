8c87e12a1a810f496671f30e4259840cc13a40b0
Joshua Flash <joshuaflash@gmail.com>
2021-12-11 14:26:37 +1300

    add tween helper extenstion class

---
+++ Source/TransformExtensions.cs
@@ -0,0 +1,19 @@
+using TweenKey.Interpolation;
+using UnityEngine;
+
+namespace TweenKey
+{
+    public static class TransformExtensions
+    {
+        private const string kPosition = "position";
+        private const string kRotation = "rotation";
+        private const string kScale = "localScale";
+        
+        public static Tween TweenMove(this Transform transform, Vector3 destination, float duration, System.Action? onComplete = null, 
+            EasingFunction? easingType = null, float delay = 0)
+        {
+            var keyFrame = new KeyFrame<Vector3>(duration, destination, easingType ?? Easing.Cubic.InOut);
+            return TweenRunner.RunTween(transform, kPosition, keyFrame, LerpFunctions.Vector3, onComplete ?? delegate {}, delay);
+        }
+    }
+}
